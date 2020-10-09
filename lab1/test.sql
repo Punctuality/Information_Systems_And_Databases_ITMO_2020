f5f6fdc51aa031b76d5f384d29c723ffbb3c75e2
Sergey <sergeyfedorov2@gmail.com>
2020-10-09 14:14:34 +0300

    hotfix

--- lab1/test.sql
+++ lab1/test.sql
@@ -11,11 +11,11 @@ INNER JOIN place to_p on to_p.place_id = movement.to_place_id;
 SELECT s.name, ac.characteristic FROM animal
 INNER JOIN species s on animal.species_id = s.species_id
 INNER JOIN animal_to_characteristic atc on animal.animal_id = atc.animal_id
-INNER JOIN animal_characteristic ac on ac.a_characteristic_id = atc.a_characteristic_id
+INNER JOIN animal_characteristic ac on ac.a_characteristic_id = atc.a_characteristic_id;
 
 -- What has it done?
 
 SELECT s.name, a.affected_by, a.action, p.place FROM animal
 INNER JOIN species s on s.species_id = animal.species_id
 INNER JOIN action a on animal.animal_id = a.animal_id
-Inner Join place p on p.place_id = a.place_id
+Inner Join place p on p.place_id = a.place_id;
